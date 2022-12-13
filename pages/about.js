b3b75adf77d23dd81371aa7ffefdd7c2ae011e3e
pratikprajapati0708 <prajapatipratik292@gmail.com>
2022-12-13 22:01:30 +0530

    First commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>THis is my about Component</div>
+  )
+}
+
+export default about
---
+++ pages/blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>This is my blog page</div>
+  )
+}
+
+export default blog
